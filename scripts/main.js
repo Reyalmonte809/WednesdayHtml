0a5886878821f9b96c57885fa998f7083bff333b
reyalmonte809 <reyalmonte89@gmail.com>
2024-04-10 13:45:45 -0400

    WIP

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+
+// DOM -document object model
+extra_special.innerHTML = "Replacement Text"
+extra_special.style.color = "red"
+
+}); //END DOM CONTENT LOADED
